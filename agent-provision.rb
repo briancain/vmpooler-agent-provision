fea5ac7a308c961bbefa837045e690435dbc9277
Brian Cain <brian.cain@puppetlabs.com>
2015-10-02 15:41:53 -0700

    Update to grab windows vms and simplify host list creation

--- agent-provision.rb
+++ agent-provision.rb
@@ -38,36 +38,48 @@ def get_os_hash_from_args(argv)
   os_hash
 end
 
-def format_os_response(body)
+def build_list(hostname_hash)
   host_list = Array.new
 
-  centos_hosts = body['centos-7-x86_64']
-  debian_hosts = body['debian-7-x86_64']
+  if hostname_hash.kind_of?(Array)
+    hostname_hash.each do |host|
+      host_list.push host
+    end
+  else
+    host_list.push hostname_hash
+  end
+
+  host_list
+end
+
+def format_os_response(body)
+  linux_host_list = Array.new
+  windows_host_list = Array.new
+  host_list = {}
+  puts body
+
+  centos_hosts = body['centos-7-x86_64']['hostname'] unless body['centos-7-x86_64'].nil?
+  debian_hosts = body['debian-7-x86_64']['hostname'] unless body['debian-7-x86_64'].nil?
+  windows_hosts = body['win-2012r2-x86_64']['hostname'] unless body['win-2012r2-x86_64'].nil?
 
   unless centos_hosts.nil?
-    centos_hosts.each do |key,host_arr|
-      if host_arr.kind_of?(Array)
-        host_arr.each do |host|
-          host_list.push host
-        end
-      else
-        host_list.push host_arr
-      end
+    centos_list = build_list(centos_hosts)
+    centos_list.each do |host|
+      linux_host_list.push host
     end
   end
 
   unless debian_hosts.nil?
-    debian_hosts.each do |key,host_arr|
-      if host_arr.kind_of?(Array)
-        host_arr.each do |host|
-          host_list.push host
-        end
-      else
-        host_list.push host_arr
-      end
+    debian_list = build_list(debian_hosts)
+    debian_list.each do |host|
+      linux_host_list.push host
     end
   end
 
+  windows_host_list = build_list(windows_hosts) unless windows_hosts.nil?
+
+  host_list['linux'] = linux_host_list
+  host_list['windows'] = windows_host_list
   host_list
 end
 
@@ -113,6 +125,9 @@ def get_token_from_file
 end
 
 def provision_hosts(pe_master, host_list)
+  linux_hosts = host_list['linux']
+  windows_hosts = host_list['windows']
+
   STDOUT.flush
   puts "Enter 'root' password for all vms (by default all pooler vms share the same password):"
   password = STDIN.noecho(&:gets).chomp
@@ -123,7 +138,13 @@ def provision_hosts(pe_master, host_list)
   run_puppet = "/opt/puppetlabs/puppet/bin/puppet agent -t"
 
 
-  host_list.each do |host|
+  # windows
+  win_user = 'Administrator'
+  get_puppet_agent = "curl -O http://agent-downloads.delivery.puppetlabs.net/2015.3/puppet-agent-latest/repos/windows/puppet-agent-1.2.5.139.g5822f8d-x64.msi"
+  add_win_node_cmd = "cmd.exe /c \"start /w msiexec /qn /L*V install.txt /i puppet-agent-1.2.5.139.g5822f8d-x64.msi PUPPET_MASTER_SERVER=#{pe_master} PUPPET_AGENT_STARTUP_MODE=Manual\""
+  run_puppet_win = "cd /cygdrive/c/Program\\ Files/Puppet\\ Labs/Puppet/bin/ && cmd /c puppet.bat agent -t"
+
+  linux_hosts.each do |host|
     begin
       ssh = Net::SSH.start(host, user, :password => password)
       puts "Setting up agent packages on host #{host}"
@@ -131,6 +152,25 @@ def provision_hosts(pe_master, host_list)
       puts setup_res
       puts "Running puppet on host #{host}"
       agent_run_res = ssh.exec!(run_puppet)
+      puts agent_run_res
+      ssh.close
+    rescue
+      STDERR.puts "Unable to connect to #{host} using #{user}"
+      exit 1
+    end
+  end
+
+  windows_hosts.each do |host|
+    begin
+      ssh = Net::SSH.start(host, win_user, :password => password)
+      puts 'Getting puppet-agent package'
+      get_package = ssh.exec!(get_puppet_agent)
+      puts get_package
+      puts "Setting up agent packages on host #{host}"
+      setup_res = ssh.exec!(add_win_node_cmd)
+      puts setup_res
+      puts "Running puppet on host #{host}"
+      agent_run_res = ssh.exec!(run_puppet_win)
       puts agent_run_res
       ssh.close
     rescue
@@ -154,14 +194,14 @@ if __FILE__ == $0
 
   if pe_master.nil?
     STDERR.puts "You did not set the PEMASTER env var"
-    STDERR.puts "Example: PEMASTER=mymasterhost.net ruby agent-provision.rb centos-7-x86_64=1 debian-7-x86_64=1"
+    STDERR.puts "Example: PEMASTER=mymasterhost.net ruby agent-provision.rb centos-7-x86_64=1 debian-7-x86_64=1 win-2012r2-x86_64=1"
   end
 
   os_hash = get_os_hash_from_args(ARGV)
 
   if os_hash.empty?
     STDERR.puts "You did not supply any arguments"
-    STDERR.puts "Example: PEMASTER=mymasterhost.net ruby agent-provision.rb centos-7-x86_64=1 debian-7-x86_64=1"
+    STDERR.puts "Example: PEMASTER=mymasterhost.net ruby agent-provision.rb centos-7-x86_64=1 debian-7-x86_64=1 win-2012r2-x86_64=1"
     exit 1
   end
 
